26e42ccedf53ea248b8b42f40034db57b7b61219
aagneye-syam <saagneye2003@gmail.com>
2023-05-29 22:07:59 +0530

    conditions added

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
 import random
 import string
 
-  
+def generate_password (min_length, number = True, special_characters = True):
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+
+    if number == True:
+        characters += digits
+    if special_characters == True:
+        characters += special  
+        
+          
+
+    
+
+generate_password(10) 
